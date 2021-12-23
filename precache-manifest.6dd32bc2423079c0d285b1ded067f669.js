d0f258cd6e2bfdd6816eeffbfaf75deb4f6a69da
diasdestarika <diasdestarika@gmail.com>
2021-12-23 16:14:18 +0700

    Updates

---
+++ precache-manifest.6dd32bc2423079c0d285b1ded067f669.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4de047df3559ffec6217f3e671d29b",
+    "url": "/TestReact/index.html"
+  },
+  {
+    "revision": "67eca78003b39d44d911",
+    "url": "/TestReact/static/css/main.b766e7bc.chunk.css"
+  },
+  {
+    "revision": "5e7c875c4cbc42a14ce0",
+    "url": "/TestReact/static/js/2.68e72f8d.chunk.js"
+  },
+  {
+    "revision": "c1823a4f1fe4309f3ff3cc56b4eb0a86",
+    "url": "/TestReact/static/js/2.68e72f8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9486afbc295e5dc37c5a",
+    "url": "/TestReact/static/js/3.6d1613db.chunk.js"
+  },
+  {
+    "revision": "19aedc330a0e6f3cd4ac3fb01654833b",
+    "url": "/TestReact/static/js/3.6d1613db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d462f4f95e97e4a3f2a",
+    "url": "/TestReact/static/js/4.8d2ee206.chunk.js"
+  },
+  {
+    "revision": "051c5f54279cbf2b3040",
+    "url": "/TestReact/static/js/5.9d9f50fc.chunk.js"
+  },
+  {
+    "revision": "67eca78003b39d44d911",
+    "url": "/TestReact/static/js/main.2254c453.chunk.js"
+  },
+  {
+    "revision": "67aa71b11f7eb7a1e204",
+    "url": "/TestReact/static/js/runtime-main.87d74e34.js"
+  },
+  {
+    "revision": "c54eef50718560d1268aa8e92025972d",
+    "url": "/TestReact/static/media/background_1920-2.c54eef50.jpg"
+  },
+  {
+    "revision": "56b6f1653e7e321c603505246f271ac0",
+    "url": "/TestReact/static/media/logo_200.56b6f165.png"
+  },
+  {
+    "revision": "4bcc13787afac1d99fc280c9bd656a2f",
+    "url": "/TestReact/static/media/sidebar-0.4bcc1378.jpg"
+  },
+  {
+    "revision": "06cae3a14d587ad810ca216f1cfdad39",
+    "url": "/TestReact/static/media/user0.06cae3a1.jpg"
+  },
+  {
+    "revision": "4f840589d7bd65aeddeec068fb94948f",
+    "url": "/TestReact/static/media/warning.4f840589.png"
+  }
+]);
